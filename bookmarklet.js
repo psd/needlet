38f7edf3c0e1f685e3b9e73b31597d0b880af3f3
psd <psd@whatfettle.com>
2014-02-04 10:42:38 +0000

    Use promises for more reliable errors

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,16 @@
 (function() {
 
-    $.getJSON(document.location + ".json", function (data) {
-        if (data.details.need_id && parseInt(data.details.need_id) > 100000) {
-            window.location = "https://maslow.production.alphagov.co.uk/needs/" + data.details.need_id;
-        }
-        else {
-          alert("No need can be found for this GOV.UK page");
-        }
-    });
+    $.getJSON(document.location + ".json")
+        .done(function (data) {
+            if (data.details.need_id && parseInt(data.details.need_id) > 100000) {
+                window.location = "https://maslow.production.alphagov.co.uk/needs/" + data.details.need_id;
+            }
+            else {
+              alert("A need has not yet been defined for this page");
+            }
+        })
+        .fail(function (data) {
+          alert("No information could be found about this page");
+        });
 
 })();
